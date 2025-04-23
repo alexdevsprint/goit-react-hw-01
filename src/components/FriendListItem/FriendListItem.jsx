e7bc0887e2ad34c58e8f78e02858cd1175f25ec7
alexdevsprint <alexdevsprint@gmail.com>
2025-04-23 16:01:26 +0300

    commit

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,9 +3,9 @@ import css from './FriendListItem.module.css'
 function FriendListItem({avatar, name, isOnline}) {
   return (
     <div>
-      <img className={css.friendItemAvatar} src={avatar} alt="Avatar" width="70" />
+      <img className={css.friendItemAvatar} src={avatar} alt="Avatar" width="55" />
       <p className={css.friendItemName}>{name}</p>
-      <p className={css.friendItemOnline}>{isOnline}</p>
+      <p className={`${css.friendItemStatus} ${isOnline ? css.friendItemOnline : css.friendItemOffline}`}>{isOnline ? 'Online' : 'Offline'}</p>
     </div>
   );
 }
